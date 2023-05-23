430ab990acb31bf5c9bf11be2d6180e83ff17e34
Penn Curtis <penn.s.m.c@gmail.com>
2023-05-23 15:06:33 -0400

    complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,32 @@
-def roll_call_dwarves():
-    pass
+dwarves_list = ['sneezy', 'sleepy', 'happy', 'doc', 'grumpy', 'dopey', 'bashful']
 
-def summon_captain_planet():
-    pass
+def roll_call_dwarves(dwarves_list):
+    i = 1
+    for dwarf in dwarves_list:
+        print(f"{i}. {dwarf}")
+        i += 1
 
-def long_planeteer_calls():
-    pass
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
-def find_the_cheese():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [f"{call.title()}!" for call in planeteer_calls]
+
+
+assorted_words = ["two", "go", "industrious", "bop"]
+
+def long_planeteer_calls(assorted_words):
+    for word in assorted_words:
+        if len(word) > 4:
+            return True
+    
+    return False
+
+
+CHEESES = ["camembert", "gouda", "cheddar"]
+
+def find_the_cheese(foods):
+    for food in foods:
+        if food in CHEESES:
+            return food
+        
+    return None
